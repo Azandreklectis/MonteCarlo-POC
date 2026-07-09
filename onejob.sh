09eadf1eff266cdecf8d3cad42310a0c1fb1aedf
Azandreklectis <phantom110049@gmail.com>
2026-07-09 09:59:38 +0000

    tweak priorty

--- onejob.sh
+++ onejob.sh
@@ -3,10 +3,12 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=48
 #SBATCH --time=01:00:00
 #SBATCH --output=job.%j.out
 #SBATCH --error=job.%j.err
+
+#SBATCH --reservation=iuac
 
 source setup_hpc.sh
 
