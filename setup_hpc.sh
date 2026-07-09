742b569d53d74e804e345c04af097186511a8372
Azandreklectis <phantom110049@gmail.com>
2026-07-09 10:11:34 +0000

    tweak priorty 2

--- setup_hpc.sh
+++ setup_hpc.sh
@@ -16,7 +16,7 @@ source /home/apps/SPACK/spack/share/spack/setup-env.sh
 # Load Compiler
 #--------------------------------------------------
 
-module load compiler/gcc/12.4
+#module load compiler/gcc/12.4
 
 #--------------------------------------------------
 # Load NVIDIA HPC SDK
@@ -25,17 +25,13 @@ module load compiler/gcc/12.4
 spack load nvhpc@24.3
 
 # Use GCC 14 runtime (needed for GLIBCXX_3.4.32)
-export GCC14_LIB=/home/apps/SPACK/spack/opt/spack/linux-cascadelake/gcc-runtime-14.2.0-xcctgudktzfgj5j7ihktrh4mzwou3quw/lib
-
-export LD_LIBRARY_PATH=$GCC14_LIB:$LD_LIBRARY_PATH
+#export GCC14_LIB=/home/apps/SPACK/spack/opt/spack/linux-cascadelake/gcc-runtime-14.2.0-xcctgudktzfgj5j7ihktrh4mzwou3quw/lib
+#
+#export LD_LIBRARY_PATH=$GCC14_LIB:$LD_LIBRARY_PATH
 
 #--------------------------------------------------
 # Load CMake
 #--------------------------------------------------
-echo
-echo "LD_LIBRARY_PATH:"
-echo "$LD_LIBRARY_PATH"
-echo
 
 spack load cmake@3.31.11
 
