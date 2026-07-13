e3b961fda0d3e9eb7c261a8d1a163054d680959c
Azandreklectis <phantom110049@gmail.com>
2026-07-13 19:54:13 +0000

    POC collapse() 2

--- main.cpp
+++ main.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 int main()
 {
-     const int N = 4;
+     const int N = 100;
+
+int a[N][N];
 
 #pragma acc parallel loop collapse(2)
-     for(int i=0;i<N;i++)
-     {
-          for(int j=0;j<N;j++)
-          {
-               printf("(%d,%d)\n",i,j);
-          }
-     }
+for(int i=0;i<N;i++)
+{
+    for(int j=0;j<N;j++)
+    {
+        a[i][j] = i + j;
+    }
+}
 }
