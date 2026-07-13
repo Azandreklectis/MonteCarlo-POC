7a3d1b1d8ad124259496e5a1481b1444491daf0e
Azandreklectis <phantom110049@gmail.com>
2026-07-13 20:02:39 +0000

    POC shared memory test

--- main.cpp
+++ main.cpp
@@ -4,16 +4,13 @@ using namespace std;
 
 int main()
 {
-     const int N = 100;
+    const int N = 100;
 
-int a[N][N];
+    int sum = 0;
 
-#pragma acc parallel loop collapse(2)
-for(int i=0;i<N;i++)
-{
-    for(int j=0;j<N;j++)
-    {
-        a[i][j] = i + j;
-    }
-}
+#pragma acc parallel loop
+    for(int i=0;i<N;i++)
+        sum++;
+
+    cout << sum << endl;
 }
