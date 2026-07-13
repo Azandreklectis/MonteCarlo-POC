066ebc559e12a16799d67bc4b5ea5e4c991ab06e
Azandreklectis <phantom110049@gmail.com>
2026-07-13 19:44:22 +0000

    POC gang vector + added output below why copyout is being called idkkk

--- main.cpp
+++ main.cpp
@@ -16,9 +16,17 @@ int main()
 
 #pragma acc update self(a)
 #pragma acc exit data delete(a)
-     
+
      for(int i=0;i<10;i++)
           cout<<a[i]<<" ";
 
      cout<<endl;
 }
+
+// 9, Generating enter data create(a[:])
+//    Generating NVIDIA GPU code
+//    14, #pragma acc loop gang, vector(96) /* blockIdx.x threadIdx.x */
+// 9, Generating implicit copyout(a[:]) [if not already present]
+//
+// 15, Generating update self(a[:])
+//    Generating exit data delete(a[:])
