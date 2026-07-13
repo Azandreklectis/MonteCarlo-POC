8c80b1076f4c1a18ebd4719123961f24d32a6152
Azandreklectis <phantom110049@gmail.com>
2026-07-13 19:37:22 +0000

    POC gang vector + tweaks

--- main.cpp
+++ main.cpp
@@ -8,12 +8,15 @@ int main()
 
      int a[N];
 
+#pragma acc enter data create(a)
+
 #pragma acc parallel loop gang vector
      for(int i=0;i<N;i++)
           a[i]=i;
 
 #pragma acc update self(a)
-
+#pragma acc exit data delete(a)
+     
      for(int i=0;i<10;i++)
           cout<<a[i]<<" ";
 
