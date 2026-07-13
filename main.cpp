57b05333f5ef3ed637b7a15293c6f9fb92038cf1
Azandreklectis <phantom110049@gmail.com>
2026-07-13 16:46:55 +0000

    POC 2 kernels

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
+     cout << "MEOWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW" <<endl;
      const int N = 10;
 
      int a[N];
 
-#pragma acc parallel loop
+#pragma acc kernels
      for (int i = 0; i < N; i++)
      {
           a[i] = i * 10;
