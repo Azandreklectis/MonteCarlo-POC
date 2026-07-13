eb18eb7f9c11eed29bc04180d281c19ed1e904b8
Azandreklectis <phantom110049@gmail.com>
2026-07-13 16:02:15 +0000

    Finally POC 1 // loop test

--- main.cpp
+++ main.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
-#include <openacc.h>
 
 using namespace std;
 
 int main()
 {
-     const int N = 16;
+     const int N = 10;
 
      int a[N];
 
 #pragma acc parallel loop
      for (int i = 0; i < N; i++)
      {
-          a[i] = i * i;
+          a[i] = i * 10;
      }
-
-     cout << "Array:\n";
 
      for (int i = 0; i < N; i++)
      {
