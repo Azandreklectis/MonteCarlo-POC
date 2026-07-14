8e101af1e2c4a4de9b6678a6c0cd4c9ca2e550b9
Azandreklectis <phantom110049@gmail.com>
2026-07-14 04:35:55 +0000

    POC of race without atomic

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-    const int N = 100;
+    const int N = 10;
 
-    int sum = 0;
+    int hist[10];
 
-#pragma acc parallel loop reduction(+:sum)
-    for(int i = 0; i < N; i++)
-        sum++;
+    for(int i=0;i<10;i++)
+        hist[i]=0;
 
-    cout << sum << endl;
+#pragma acc parallel loop
+    for(int i=0;i<N;i++)
+    {
+        int bin = i % 10;      // deterministic instead of random
+        hist[bin]++;
+    }
+
+    int total = 0;
+
+    for(int i=0;i<10;i++)
+    {
+        cout << "Bin " << i << " : " << hist[i] << endl;
+        total += hist[i];
+    }
+
+    cout << "\nTotal = " << total << endl;
 }
