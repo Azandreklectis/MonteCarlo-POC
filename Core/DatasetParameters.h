60638ed95aa16b8b1c86b2f8b1873fb27a72ce2c
Azandreklectis <phantom110049@gmail.com>
2026-07-31 09:45:09 +0000

    tweaks

--- Core/DatasetParameters.h
+++ Core/DatasetParameters.h
@@ -12,8 +12,10 @@ struct DatasetParameters
     // Temperature Sweep
     //==================================================
 
-    double startTemperature = 0.1;
-    double endTemperature = 1.0;
+    double startTemperature = 0.1; // 0.0
+
+    double endTemperature = 10; // 10.0
+
     double temperatureStep = 0.1;
 
     //==================================================
@@ -21,7 +23,9 @@ struct DatasetParameters
     //==================================================
 
     double startMagneticField = 0.0;
-    double endMagneticField = 0.0;
+
+    double endMagneticField = 10.0;
+
     double magneticFieldStep = 0.1;
 
     //==================================================
@@ -30,12 +34,16 @@ struct DatasetParameters
 
     double couplingConstant = 1.0;
 
+    double magneticField = 0.55;
+
     //==================================================
     // Monte Carlo Parameters
     //==================================================
 
     int monteCarloSteps = 5000;
+
     int thermalizationSteps = 2000;
+
     int measurementInterval = 10;
 
     //==================================================
