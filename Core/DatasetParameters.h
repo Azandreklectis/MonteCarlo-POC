102abb2e3fe38734f5ed584ce17ae9e7ad6cf53c
Azandreklectis <phantom110049@gmail.com>
2026-08-01 17:25:25 +0000

    therma 100 k T 2.2

--- Core/DatasetParameters.h
+++ Core/DatasetParameters.h
@@ -12,9 +12,9 @@ struct DatasetParameters
     // Temperature Sweep
     //==================================================
 
-    double startTemperature = 1.5; // 0.0
+    double startTemperature = 2.2; // 0.0
 
-    double endTemperature = 1.5; // 10.0
+    double endTemperature = 2.2; // 10.0
 
     double temperatureStep = 0.5;
 
